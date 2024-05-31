f9ce06129bfd4b6990e006e984034154f4ea27d1
Virajmandanka <virajmandanka6@gmail.com>
2024-05-31 15:03:11 +0530

    React App

---
+++ day1/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import De from './section';
+
+
+
+function App() {
+  return (
+    <div className="App">
+  
+      <De/>
+
+    </div>
+  );
+}
+
+export default App;
+
+
---
+++ day1/src/section.jsx
@@ -0,0 +1,77 @@
+
+function De()
+{
+
+return(
+
+    <div className="viraj">
+       
+       <div class="men3">
+        <p class="q7">Popular Item in the market</p>
+        <h1 class="h4">Trending Product</h1>
+        <hr class="hr"/>
+    </div>
+    <div class="men4">
+        <div class="box6"><img src="image/product1.png.webp" alt=""/>
+            <div class="box8">
+                <div class="i1"><i class="fa-solid fa-magnifying-glass"></i></div>
+                <div class="i1"><i class="fa-solid fa-cart-shopping"></i></div>
+                <div class="i1"><i class="fa-regular fa-heart"></i></div>
+            </div>
+        </div>
+        <div class="box6"><img src="image/product2.png.webp" alt=""/>
+            <div class="box8">
+                <div class="i1"><i class="fa-solid fa-magnifying-glass"></i></div>
+                <div class="i1"><i class="fa-solid fa-cart-shopping"></i></div>
+                <div class="i1"><i class="fa-regular fa-heart"></i></div>
+            </div>
+        </div>
+        <div class="box6"><img src="image/product3.png.webp" alt=""/>
+            <div class="box8">
+                <div class="i1"><i class="fa-solid fa-magnifying-glass"></i></div>
+                <div class="i1"><i class="fa-solid fa-cart-shopping"></i></div>
+                <div class="i1"><i class="fa-regular fa-heart"></i></div>
+            </div>
+        
+        </div>
+        <div class="box6"><img src="image/product4.png.webp" alt=""/>
+            <div class="box8">
+                <div class="i1"><i class="fa-solid fa-magnifying-glass"></i></div>
+                <div class="i1"><i class="fa-solid fa-cart-shopping"></i></div>
+                <div class="i1"><i class="fa-regular fa-heart"></i></div>
+            </div>
+        
+        </div>
+
+        <div class="box7">
+            <p class="q8">Accessories</p>
+            <h1 class="h5">Quartz Belt Watch</h1>
+            <h1 class="h6">$150.00</h1>
+        </div>
+        <div class="box7">
+            <p class="q8">Beauty</p>
+            <h1 class="h5">Women Freshwash</h1>
+            <h1 class="h6">$150.00</h1>
+        </div>
+        <div class="box7">
+            <p class="q8">Decor</p>
+            <h1 class="h5">Room Flash Light</h1>
+            <h1 class="h6">$150.00</h1>
+        </div>
+        <div class="box7">
+            <p class="q8">Decor</p>
+            <h1 class="h5">Room Flash Light</h1>
+            <h1 class="h6">$150.00</h1>
+        </div>
+
+
+    </div>
+
+
+
+    </div>
+)
+
+
+}
+export default De;
